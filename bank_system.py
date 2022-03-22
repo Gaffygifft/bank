ee9591c5c8f0137112cc505e9ff18d2dfd1b779f
Daramola Gafaru Olalekan <64953191+Gaffygifft@users.noreply.github.com>
2022-03-22 14:08:35 +0100

    Update Banking

--- bank_system.py
+++ bank_system.py
@@ -1,2 +1,17 @@
 
-
+print('Choose your time either in sec,min or hour')
+rate = int(input('choose your rate >> '))
+hour = int(input('chosose your hour >> '))
+pick_time = input('Input time in sec,min or hour :')
+if pick_time == 'sec':
+    hour = hour/3600
+    pay = rate * hour
+    
+elif(pick_time) == 'min':
+        hour = hour/60
+        pay = rate * hour
+        
+else:
+    pay = hour * rate
+    
+print(pay)
