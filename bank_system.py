220fa5adeb536333d68607e867a8f4250a3af4af
Daramola Gafaru Olalekan <64953191+Gaffygifft@users.noreply.github.com>
2022-03-22 14:11:53 +0100

    Updated part

--- bank_system.py
+++ bank_system.py
@@ -15,3 +15,27 @@ else:
     pay = hour * rate
     
 print(pay)
+
+class Bank_System:
+  
+  def __init__(self):
+    self.balance = 1000
+    self.credit = 0
+    self.debit = 0
+    
+  def take_amount(self):
+    self.credit = int(input(">>"))
+    self.debit = int(input(">>"))
+    
+  def deposit(self):
+    if self.credit < 1000:
+      print("You can only deposit above 1000")
+    else:
+      self.amount = self.balance + self.credit
+      print("Your balance is now :",self.amount)
+      
+bank = Bank_System()
+
+print(bank.take_amount())
+print(bank.deposit())
+    
